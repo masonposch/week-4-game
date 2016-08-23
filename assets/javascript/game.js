a5ef28e47233a02c892563e313799c776bdeb165
masonposch <masonposch@Mason-Poschs-MacBook-Pro.local>
2016-08-23 01:01:31 -0500

    more JS

---
+++ assets/javascript/game.js
@@ -0,0 +1,40 @@
+var attack = 8;
+
+var attackIncrease = 1;
+
+var counterAttack = 25;
+
+var firstCharacter = document.getElementById("firstCharacter").value;
+
+var secondCharacter = document.getElementById("secondCharacter").value;
+
+var thirdCharacter = document.getElementById("thirdCharacter").value;
+
+var fourthCharacter = document.getElementById("fourthCharacter").value;
+
+firstCharacter = {
+	name: "Obi Wan Kenobi",
+	points: 120;
+};
+
+secondCharacter = {
+	name: "QuiGon Jinn",
+	points: 100; 
+};
+
+thirdCharacter = {
+	name: "KyloRen",
+	points: 150
+};
+
+fourthCharacter = {
+	name: "Darth Vader",
+	points: 180
+};
+
+var allCharacters = [firstCharacter, secondCharacter, thirdCharacter, fourthCharacter];
+
+
+var beginGame = function(){
+
+}
